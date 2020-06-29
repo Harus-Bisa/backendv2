0ec5aece03da437ceff6d3cba53ff7990b659d4c
Wilson Burnawan <wilson.burnawan@gmail.com>
2020-06-29 16:50:47 -0700

    change variable name

--- src/routes/reviewee.controller.js
+++ src/routes/reviewee.controller.js
@@ -9,177 +9,190 @@ const revieweeService = new RevieweeService();
 const userService = new UserService();
 
 router.post('/', authentication, async (req, res) => {
-  if (!req.authenticated) {
-    res.statusMessage = 'Authentication is required to create new review.';
-    return res.status(401).end();
-  }
+	if (!req.authenticated) {
+		res.statusMessage = 'Authentication is required to create new review.';
+		return res.status(401).end();
+	}
 
-  try {
-    const revieweeData = {
-      name: req.body.name,
-      school: req.body.school,
-      review: req.body.review,
-      courseName: req.body.courseName,
-      overallRating: req.body.overallRating,
-      recommendationRating: req.body.recommendationRating,
-      difficultyRating: req.body.difficultyRating,
-      yearTaken: req.body.yearTaken,
-      grade: req.body.grade,
-      tags: req.body.tags,
-      textbookRequired: req.body.textbookRequired,
-      teachingStyles: req.body.teachingStyles,
-    };
+	try {
+		const revieweeData = {
+			name: req.body.name,
+			school: req.body.school,
+			review: req.body.review,
+			courseName: req.body.courseName,
+			overallRating: req.body.overallRating,
+			recommendationRating: req.body.recommendationRating,
+			difficultyRating: req.body.difficultyRating,
+			yearTaken: req.body.yearTaken,
+			grade: req.body.grade,
+			tags: req.body.tags,
+			textbookRequired: req.body.textbookRequired,
+			teachingStyles: req.body.teachingStyles,
+		};
 
 		const authorId = req.userId;
-    const { newReviewee } = await revieweeService.createRevieweeWithReview(
-      authorId, revieweeData
-    );
+		const { newReviewee } = await revieweeService.createRevieweeWithReview(
+			authorId,
+			revieweeData
+		);
 
-    res.statusMessage = 'Create review for new reviewee is successful.';
-    return res.status(201).send(newReviewee);
-  } catch (err) {
-    console.log(err);
-    res.statusMessage =
-      'There was an error creating a review for new reviewee.';
-    return res.status(500).end();
-  }
+		res.statusMessage = 'Create review for new reviewee is successful.';
+		return res.status(201).send(newReviewee);
+	} catch (err) {
+		console.log(err);
+		res.statusMessage =
+			'There was an error creating a review for new reviewee.';
+		return res.status(500).end();
+	}
 });
 
 router.get('/', async (req, res) => {
-  try {
-    const { reviewees, totalReviewees } = await revieweeService.getRevieweesByName(
-      req.query.name,
-      req.query.school,
-      req.query.index,
-			req.query.limit,
-			req.query.sortBy,
-			req.query.ascending
-    );
-    res.statusMessage =
-      'Get list of reviewees matching the query is successful.';
-    return res.status(200).send({reviewees, totalReviewees});
-  } catch (err) {
-    console.log(err);
-    res.statusMessage =
-      'There was an error getting the reviewees matching the query.';
-    return res.status(500).end();
-  }
+	try {
+		const name = req.query.name;
+		const school = req.query.school;
+		const index = req.query.index;
+		const limit = req.query.limit;
+		const sortBy = req.query.sortBy;
+		const ascending = req.query.ascending;
+
+		const {
+			reviewees,
+			totalReviewees,
+		} = await revieweeService.getRevieweesByName(
+			name,
+			school,
+			index,
+			limit,
+			sortBy,
+			ascending
+		);
+		res.statusMessage =
+			'Get list of reviewees matching the query is successful.';
+
+		return res.status(200).send({ reviewees, totalReviewees });
+		
+	} catch (err) {
+		console.log(err);
+		res.statusMessage =
+			'There was an error getting the reviewees matching the query.';
+		return res.status(500).end();
+	}
 });
 
 router.get('/:revieweeId', authentication, async (req, res) => {
-  try {
-    const { reviewee } = await revieweeService.getRevieweeById(
-      req.params.revieweeId,
-      req.authenticated,
-      req.userId
-    );
-    if (reviewee) {
-      res.statusMessage = 'Get reviewee is successful.';
-      return res.status(200).send(reviewee);
-    } else {
-      res.statusMessage = 'Reviewee not found.';
-      return res.status(404).end();
-    }
-  } catch (err) {
-    console.log(err);
-    res.statusMessage = 'There was an error getting the reviewee.';
-    return res.status(500).end();
-  }
+	try {
+		const { reviewee } = await revieweeService.getRevieweeById(
+			req.params.revieweeId,
+			req.authenticated,
+			req.userId
+		);
+		if (reviewee) {
+			res.statusMessage = 'Get reviewee is successful.';
+			return res.status(200).send(reviewee);
+		} else {
+			res.statusMessage = 'Reviewee not found.';
+			return res.status(404).end();
+		}
+	} catch (err) {
+		console.log(err);
+		res.statusMessage = 'There was an error getting the reviewee.';
+		return res.status(500).end();
+	}
 });
 
 router.post('/:revieweeId/reviews', authentication, async (req, res) => {
-  if (!req.authenticated) {
-    res.statusMessage = 'Authentication is required to create new review.';
-    return res.status(401).end();
-  }
-  try {
-    const { revieweeId, newReview } = await revieweeService.createReview(
-      req.params.revieweeId,
-      req.body
-    );
-    if (revieweeId) {
-      const { outgoingReview } = await userService.addOutgoingReview(
-        req.userId,
-        revieweeId,
-        newReview.reviewId
-      );
-      res.statusMessage = 'Create review for a reviewee is successful.';
-      return res.status(201).send(newReview);
-    } else {
-      res.statusMessage = 'Reviewee not found.';
-      return res.status(404).end();
-    }
-  } catch (err) {
-    console.log(err);
-    res.statusMessage = 'There was an error creating the review.';
-    return res.status(500).end();
-  }
+	if (!req.authenticated) {
+		res.statusMessage = 'Authentication is required to create new review.';
+		return res.status(401).end();
+	}
+	try {
+		const { revieweeId, newReview } = await revieweeService.createReview(
+			req.params.revieweeId,
+			req.body
+		);
+		if (revieweeId) {
+			const { outgoingReview } = await userService.addOutgoingReview(
+				req.userId,
+				revieweeId,
+				newReview.reviewId
+			);
+			res.statusMessage = 'Create review for a reviewee is successful.';
+			return res.status(201).send(newReview);
+		} else {
+			res.statusMessage = 'Reviewee not found.';
+			return res.status(404).end();
+		}
+	} catch (err) {
+		console.log(err);
+		res.statusMessage = 'There was an error creating the review.';
+		return res.status(500).end();
+	}
 });
 
 router.post(
-  '/:revieweeId/reviews/:reviewId/:vote',
-  authentication,
-  async (req, res) => {
-    if (!req.authenticated) {
-      res.statusMessage = 'Authentication is required to add vote.';
-      return res.status(401).end();
-    }
+	'/:revieweeId/reviews/:reviewId/:vote',
+	authentication,
+	async (req, res) => {
+		if (!req.authenticated) {
+			res.statusMessage = 'Authentication is required to add vote.';
+			return res.status(401).end();
+		}
 
-    try {
-      const {
-        cancelVote,
-        switchVote,
-        selectedVote,
-        user,
-      } = await userService.updateHelpfulnessVote(
-        req.userId,
-        req.params.revieweeId,
-        req.params.reviewId,
-        req.params.vote
-      );
+		try {
+			const {
+				cancelVote,
+				switchVote,
+				selectedVote,
+				user,
+			} = await userService.updateHelpfulnessVote(
+				req.userId,
+				req.params.revieweeId,
+				req.params.reviewId,
+				req.params.vote
+			);
 
-      if (!user) {
-        res.statusMessage =
-          'There was an error finding the user associated with the authentication token.';
-        return res.status(401).end();
-      }
+			if (!user) {
+				res.statusMessage =
+					'There was an error finding the user associated with the authentication token.';
+				return res.status(401).end();
+			}
 
-      const { votedReview } = await revieweeService.updateHelpfulnessVote(
-        cancelVote,
-        switchVote,
-        selectedVote.revieweeId,
-        selectedVote.reviewId,
-        selectedVote.vote,
-        req.userId
-      );
+			const { votedReview } = await revieweeService.updateHelpfulnessVote(
+				cancelVote,
+				switchVote,
+				selectedVote.revieweeId,
+				selectedVote.reviewId,
+				selectedVote.vote,
+				req.userId
+			);
 
-      if (votedReview) {
-        // check is duplicate TODO
-        res.statusMessage = 'Add helpfulness vote to review is successful.';
-        return res.status(201).send(votedReview);
-      } else {
-        // cancel adding helpfulness vote to user
-        const {
-          cancelVote,
-          switchVote,
-          selectedVote,
-        } = await userService.updateHelpfulnessVote(
-          req.userId,
-          req.params.revieweeId,
-          req.params.reviewId,
-          req.params.vote
-        );
+			if (votedReview) {
+				// check is duplicate TODO
+				res.statusMessage = 'Add helpfulness vote to review is successful.';
+				return res.status(201).send(votedReview);
+			} else {
+				// cancel adding helpfulness vote to user
+				const {
+					cancelVote,
+					switchVote,
+					selectedVote,
+				} = await userService.updateHelpfulnessVote(
+					req.userId,
+					req.params.revieweeId,
+					req.params.reviewId,
+					req.params.vote
+				);
 
-        res.statusMessage = 'Review not found.';
-        return res.status(404).end();
-      }
-    } catch (err) {
-      console.log(err);
-      res.statusMessage =
-        'There was an error adding helpfullness vote to the review.';
-      return res.status(500).end();
-    }
-  }
+				res.statusMessage = 'Review not found.';
+				return res.status(404).end();
+			}
+		} catch (err) {
+			console.log(err);
+			res.statusMessage =
+				'There was an error adding helpfullness vote to the review.';
+			return res.status(500).end();
+		}
+	}
 );
 
 module.exports = router;
